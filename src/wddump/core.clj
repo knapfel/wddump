1098dd8972aabf708d310143405d416221fc3a89
knapfel <napf@live.com>
2015-10-23 15:56:04 +0200

    added labels

--- src/wddump/core.clj
+++ src/wddump/core.clj
@@ -5,9 +5,6 @@
 
 (use 'clojure.java.io)
 
-(defn format-fact [s p o]
-  (str s ", " (subs (str p) 1) ", Q" o))
-
 (defn get-simple-facts 
   "Get the simple item-valued facts from a claim" [mainsnaks]
   (remove nil? 
@@ -17,14 +14,21 @@
                  (get v :numeric-id)))                
             mainsnaks)))
 
-(defn print-simple-facts [line]
-  (let [entity (json/read-str line :key-fn keyword)]
-    (doseq [claim (get entity :claims)]           
-      (doseq [fact (get-simple-facts (val claim))]
-        (println (format-fact (get entity :id) (key claim) fact))))))
+(defn format-simple-fact [s p o]
+  (str "fact, " s ", " (subs (str p) 1) ", Q" o))
+
+(defn print-simple-facts [entity]
+  (doseq [claim (get entity :claims)]           
+    (doseq [fact (get-simple-facts (val claim))]
+      (println (format-simple-fact (get entity :id) (key claim) fact)))))
+
+(defn print-label [entity]
+  (println "label, "(get entity :id) ", " (.replaceAll (get-in entity [:labels :en :value])  "," "" )))
 
 ;; or: (FileReader. "resources/wdsnip.json")
 (defn -main  [& args]
   (with-open [rdr (BufferedReader. *in*)]
     (doseq [line (drop-last (drop 1 (line-seq rdr)))]
-      (print-simple-facts line))))
+      (let [entity (json/read-str line :key-fn keyword)] 
+        (print-label entity)        
+        (print-simple-facts entity)))))
